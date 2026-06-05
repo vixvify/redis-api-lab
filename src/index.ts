57bd76bfe21c94bfa6e160a86c571ae3c82abd31
😺 <asnaweeezor@gmail.com>
2026-06-05 10:02:56 +0700

    feat: initialize Elysia server, Postgres database schema, and update Docker port mapping

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const app = new Elysia()
   .use(userRoutes)
   .get("/", () => "Hello Elysia")
   .listen(3000);
-
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
